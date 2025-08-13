b5b5e680ce5cd0076270b282efc6afc1c4e398a2
mbxelle <mbxelle@gmail.com>
2025-08-13 14:12:20 -0400

    WIP(add to google calendar feature): added javascript code to turn html event date and time to google calendar date format

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ let day = ev.querySelector('.event-day').textContent.trim();
 let title = ev.querySelector('.event-name').textContent.trim();
 let desc = ev.querySelector('.event-discription').textContent.trim();
 let timeText = ev.querySelector('.event-time').textContent;
-}
+
+//note: GOOGLE CALENDAR NEEDS THIS FORMAT: YYYYMMDDTHHMMSS/YYYYMMDDTHHMMSS TO SAVE CORRECT DATE AND TIME
+
+//turns month name into month number
+let monthNum = new Date(`${monthName} 1, ${new Date().getFullYear()}`).getMonth() + 1;
+//make sure it is two digits and turns number into string 
+monthNum = String(monthNum).padStart(2, '0');
+//build final date string in format YYYYMMDD 
+let dateStr = `${new Date().getFullYear()}${monthNum}${day.padStart(2,'0')}`;
+
+})
