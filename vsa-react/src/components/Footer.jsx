88b733c3f0c9ce9b735cd525bdfb8983dd9b96dc
mbxelle <mbxelle@gmail.com>
2025-12-27 15:11:30 -0500

    added grey social media buttons to footer seciton

--- vsa-react/src/components/Footer.jsx
+++ vsa-react/src/components/Footer.jsx
@@ -8,16 +8,16 @@ export default function Footer() {
       <p>{t("footer.copyright")}</p>
       <div className="footer-social" aria-label="Social media">
         <a href="https://www.instagram.com/torontometvsa/?hl=en" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-          <img src="/instagram.png" alt="Instagram logo" />
+          <img src="/grey_insta.png" alt="Instagram logo" />
         </a>
         <a href="https://www.facebook.com/TorontoMetVSA/" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-          <img src="/facebook.png" alt="Facebook logo" />
+          <img src="/grey_facebook.png" alt="Facebook logo" />
         </a>
         <a href="https://www.tiktok.com/@tmuvsa" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
-          <img src="/tiktok.png" alt="TikTok logo" />
+          <img src="/grey_tiktok.png" alt="TikTok logo" />
         </a>
         <a href="https://ca.linkedin.com/company/torontometvsa" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-          <img src="/linkedin.png" alt="LinkedIn logo" />
+          <img src="/grey_linkedin.png" alt="LinkedIn logo" />
         </a>
       </div>
     </footer>
